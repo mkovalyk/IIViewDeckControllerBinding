09c3c8f3088088119c83d20f42ffb560299f2342
Michael Kovalyk <mishakovalyk@gmail.com>
2013-02-27 16:13:43 +0200

    Added new files
    New files

---
+++ src/binding/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System;
+using MonoTouch.ObjCRuntime;
+
+[assembly: LinkWith ("libIIViewDeckController.a", LinkTarget.Simulator|LinkTarget.ArmV7, ForceLoad = true, Frameworks = "CoreGraphics")]
+
